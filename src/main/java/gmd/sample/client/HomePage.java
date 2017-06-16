3bf7e95774d550cfc16f3acf4729ac289aa1a993
cupi <chuong.vo@jmango360.com>
2017-06-16 15:15:55 +1000

    confirmed the endless looping issue being fixed by cleaning the SuperDevMode cache

--- src/main/java/gmd/sample/client/HomePage.java
+++ src/main/java/gmd/sample/client/HomePage.java
@@ -1,9 +1,16 @@
 package gmd.sample.client;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.SuggestOracle;
 import com.google.gwt.user.client.ui.Widget;
+
+import gwt.material.design.addins.client.autocomplete.MaterialAutoComplete;
+import gwt.material.design.client.ui.MaterialDatePicker;
 
 public class HomePage extends Composite {
 
@@ -11,7 +18,23 @@ public class HomePage extends Composite {
 	}
 	private static Binder uiBinder = GWT.create(Binder.class);
 
+	@UiField
+	MaterialAutoComplete autoCompleteField;
+	@UiField
+	MaterialDatePicker dateField;
+	
 	public HomePage() {
 		initWidget(uiBinder.createAndBindUi(this));
+		dateField.setValue(new Date());
+		UserSuggestion suggestions = new UserSuggestion();
+		autoCompleteField.setSuggestions(suggestions);
+	}
+
+	private class UserSuggestion extends SuggestOracle {
+
+		@Override
+		public void requestSuggestions(Request request, Callback callback) {
+			// TODO Auto-generated method stub
+		}
 	}
 }
